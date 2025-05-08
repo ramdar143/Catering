e4d601858fa266140f228beb090d79d5f1b75027
ramdar143 <161620038+ramdar143@users.noreply.github.com>
2025-05-08 18:01:46 +0800

    Delete users directory

--- users/apps.py
+++
@@ -1,9 +0,0 @@
-# users/apps.py
-
-from django.apps import AppConfig
-
-class UsersConfig(AppConfig):
-    name = 'users'
-
-    def ready(self):
-        import users.signals  # Ensure the signals are loaded when the app is ready
