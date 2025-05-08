72c676c88819f44f4ca439ae6affaf25303ee37c
ramdar143 <161620038+ramdar143@users.noreply.github.com>
2025-05-08 18:00:48 +0800

    Delete delivery directory

--- delivery/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Delivery
-
-@admin.register(Delivery)
-class DeliveryAdmin(admin.ModelAdmin):
-    list_display = ('order', 'assigned_driver', 'delivery_time', 'status')
-    list_filter = ('status', 'delivery_time')
-    search_fields = ('tracking_number',)
--- delivery/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('assign_delivery/<int:pk>/', views.assign_delivery, name='assign_delivery'),
-    path('update_delivery/<int:pk>/', views.update_delivery, name='update_delivery'),
-]
