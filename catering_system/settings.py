2d330567d389946bf4e96daf74fdfee65028a76f
ramdar143 <161620038+ramdar143@users.noreply.github.com>
2025-05-08 17:31:37 +0800

    Update settings.py

--- catering_system/settings.py
+++ catering_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-13$ck(9^9#3&yx38m381-9u9*^prs4lx8o!&$he2q!4v=#04%f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOST = ['.vercel.app']
 
 
 # Application definition
