b96b78176e251a6e8047a766bd928ba96989b6a2
ramdar143 <161620038+ramdar143@users.noreply.github.com>
2025-05-08 17:15:09 +0800

    Delete orders directory

--- orders/bulk_orders/views.py
+++
@@ -1,6 +0,0 @@
-# bulk_orders/views.py
-from django.shortcuts import render
-
-def bulk_order_list(request):
-    # You can replace this with actual bulk order logic later
-    return render(request, 'bulk_orders/bulk_order_list.html')
