c007f149bd7fe64dccb8a11bf5fd7e1b8e6ba5a1
ramdar143 <161620038+ramdar143@users.noreply.github.com>
2025-05-08 18:01:19 +0800

    Delete orders directory

--- orders/bulk_orders/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import BulkOrder
-
-@admin.register(BulkOrder)
-class BulkOrderAdmin(admin.ModelAdmin):
-    list_display = ('base_order', 'frequency', 'next_delivery_date')
-    list_filter = ('frequency',)
